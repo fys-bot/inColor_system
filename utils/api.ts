1235a79e448dbe1abe08602fbfbf33fd36e7d0d2
derrick <2849806368@qq.com>
2026-03-10 16:40:01 +0800

    update host name ip

--- utils/api.ts
+++ utils/api.ts
@@ -2,4 +2,4 @@
  * API 基础地址配置
  * 局域网测试时写死 IP，后续改回动态获取：`http://${window.location.hostname}:4000`
  */
-export const API_BASE = `http://10.10.88.135:4000`;
+export const API_BASE = `http://${window.location.hostname}:4000`;
